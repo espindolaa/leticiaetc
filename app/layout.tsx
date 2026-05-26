3ee4eb57374b1f0154740c2c9e3e465751944241
espindolaa <lucas.espindola@hotmail.com>
2026-05-26 14:27:25 -0300

    we crazy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
 import './globals.css'
-import TagManager from 'react-gtm-module'
- 
-const tagManagerArgs = {
-    gtmId: 'G-FLSQ0ZN7JK'
-}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  if (process.browser) {
-    TagManager.initialize(tagManagerArgs);
-  }
-
   return (
     <html lang="en">
       <title>leticia, etc: vitrine</title>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FLSQ0ZN7JK"></script>
+        <script 
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+                     function gtag(){dataLayer.push(arguments);}
+                     gtag('js', new Date());
+                     gtag('config', 'G-FLSQ0ZN7JK');`,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
